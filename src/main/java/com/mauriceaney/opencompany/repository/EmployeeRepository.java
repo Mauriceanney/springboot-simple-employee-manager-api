b558d0941460d3107c45acefb7a229de0654fe55
Maurice Aney <mauriceaneyy@gmail.com>
2021-07-09 16:28:33 +0000

    add Jpa anotation to employee resource repository

--- src/main/java/com/mauriceaney/opencompany/repository/EmployeeRepository.java
+++ src/main/java/com/mauriceaney/opencompany/repository/EmployeeRepository.java
@@ -2,11 +2,13 @@ package com.mauriceaney.opencompany.repository;
 
 import com.mauriceaney.opencompany.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Maurice Aney
  * @date 09/07/2021 15:54
  */
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee findEmployeeById(Long Id);
 }
