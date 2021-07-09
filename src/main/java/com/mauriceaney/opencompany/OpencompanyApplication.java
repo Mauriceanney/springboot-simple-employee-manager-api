6fd2b321b075d37ad93a032e846459cece53dc72
Maurice Aney <mauriceaneyy@gmail.com>
2021-07-09 15:48:30 +0000

    initial commit

---
+++ src/main/java/com/mauriceaney/opencompany/OpencompanyApplication.java
@@ -0,0 +1,13 @@
+package com.mauriceaney.opencompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpencompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mauriceaney/opencompany/OpencompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauriceaney.opencompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
