34dc677a593755809b82f1ee62b21bb0a2f8d9d5
Maurice Aney <mauriceaneyy@gmail.com>
2021-07-13 17:21:11 +0000

    add find all and find by id method

---
+++ src/main/java/com/mauriceaney/opencompany/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.mauriceaney.opencompany.controller;
+
+import com.mauriceaney.opencompany.model.Employee;
+import com.mauriceaney.opencompany.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Maurice Aney
+ * @date 12/07/2021 17:30
+ */
+@RestController
+@RequestMapping("/api/v1/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+    @GetMapping("/")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+}
--- src/main/java/com/mauriceaney/opencompany/service/EmployeeService.java
+++ src/main/java/com/mauriceaney/opencompany/service/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.mauriceaney.opencompany.service;
 
+import com.mauriceaney.opencompany.model.Employee;
 import com.mauriceaney.opencompany.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Maurice Aney
@@ -11,7 +15,27 @@ import org.springframework.stereotype.Service;
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+
+    @Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
+
+    /**
+     * @return List<Employee> employee list
+     */
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    /**
+     *
+     * @param employeeId id of employee to find
+     * @return Employee a single employee with given
+     */
+    public Employee findEmployeeById(Long employeeId){
+        return employeeRepository.findEmployeeById(employeeId);
+    }
+
+
 }
